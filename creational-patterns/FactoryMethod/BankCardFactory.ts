00819877510b6a41770ebf02be3d9d1aab5a862a
mariam.mahareeq <mariam.mahareeq@transcend.ps>
2023-06-29 00:56:36 +0300

    implement the Factory mothed

--- creational-patterns/FactoryMethod/BankCardFactory.ts
+++ creational-patterns/FactoryMethod/BankCardFactory.ts
@@ -3,7 +3,12 @@ import {MasterCard} from './MasterCard';
 import { BankCard } from './BankCard.interface';
 
 export class BankCardFactory {
-
+  
+  /* - Factory Method: is a creational design pattern that allow to create objects without 
+     specificy type of object directly. 
+     
+     - we will not create object via a new keyword but it is being done through factory method;
+     */
   public createBankCard(card_type: string): BankCard{
     if(card_type === 'visa')
       return new VisaCard();
